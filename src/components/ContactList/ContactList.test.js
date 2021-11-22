95909cf94bcc764569c34e5b059ac1de68acf23e
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-11-22 09:59:51 +0200

    Tests for ContactList component have been updated

--- src/components/ContactList/ContactList.test.js
+++ src/components/ContactList/ContactList.test.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
+import ContactList from './ContactList';
 
-// const mockDispatch = jest.fn();
-// const mockUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
+describe('ContactList component', () => {
+  const props = {
+    contactsData: [
+      {
+        id: '1',
+        name: 'Gendalf',
+        number: '355-78-19',
+      },
+    ],
+  };
 
-const deleteContactMock = jest.fn();
-jest.mock('../../redux/actions.js', () => ({
-  deleteContact: deleteContactMock,
-}));
+  const deleteContactClickMock = jest.fn();
 
-describe('ContactList', () => {
-  it('deleteContact dispatch action', () => {
-    expect(deleteContactMock).toHaveBeenCalled();
+  const component = shallow(
+    <Provider store={store}>
+      <ContactList {...props} />
+    </Provider>,
+  );
+
+  it('should render with props', () => {
+    expect(component.exists()).toBeTruthy();
+  });
+
+  it('should be empty without props', () => {
+    const props = [];
+    const component = shallow(
+      <Provider store={store}>
+        <ContactList {...props} />
+      </Provider>,
+    );
+    const ul = component.find('ul');
+    const li = ul.find('li');
+    expect(li).toHaveLength(0);
+  });
+
+  it('click on delete contact button', () => {
+    const li = shallow(
+      <li>
+        <button
+          onClick={() => deleteContactClickMock(props.contactsData.id)}
+        ></button>
+      </li>,
+    );
+    li.find('button').simulate('click');
+    expect(deleteContactClickMock).toHaveBeenCalled();
   });
 });
