cfcb889f16cd71f0c5b255a95a4d39e827132f42
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-11-29 19:53:31 +0200

    Tests for ContactList component have been updated

--- src/components/ContactList/ContactList.test.js
+++ src/components/ContactList/ContactList.test.js
@@ -8,44 +8,36 @@ describe('ContactList component', () => {
   const useSelectorMock = jest.spyOn(redux, 'useSelector');
   const useDispatchMock = jest.spyOn(redux, 'useDispatch');
 
-  const dispatchMock = jest.fn();
   const deleteContactClickMock = jest.fn();
-  const props = { contactsData: contactsMock };
-  const propsWithOnClick = {
+  const props = {
     contactsData: contactsMock,
-    onClick: () => deleteContactClickMock(props.contactsData.id),
+    onClick: deleteContactClickMock(),
   };
 
-  it('should render with props', () => {
-    useSelectorMock.mockReturnValue({ error: '' });
-    useDispatchMock.mockReturnValue(dispatchMock);
+  useSelectorMock.mockReturnValue({ error: '' });
+  useDispatchMock.mockReturnValue(jest.fn());
+  const contactList = shallow(<ContactList {...props} />);
 
-    const component = shallow(<ContactList {...props} />);
-    expect(component.exists()).toBeTruthy();
+  it('should render with props', () => {
+    expect(contactList.exists()).toBeTruthy();
+  });
+
+  it('delete contact button renders in each contact', () => {
+    const contactListButton = contactList.find('button');
+    expect(contactListButton).toHaveLength(3);
+  });
+
+  it('click on delete contact button', () => {
+    const contactListButton = contactList.find('button').at(1);
+    contactListButton.simulate('click');
+    expect(deleteContactClickMock).toHaveBeenCalledTimes(1);
   });
 
   it('should be empty without props', () => {
     const props = { contactsData: [] };
-    const component = shallow(<ContactList {...props} />);
-    const ul = component.find('ul');
+    contactList.setProps(props);
+    const ul = contactList.find('ul');
     const li = ul.find('li');
     expect(li).toHaveLength(0);
-  });
-
-  it('delete contact button renders in each contact', () => {
-    const contactListComponent = shallow(<ContactList {...propsWithOnClick} />);
-    const liButtonElement = contactListComponent.find('button');
-    expect(liButtonElement).toHaveLength(3);
-  });
-
-  // Question - Received number of calls: 0 - async deleteContactClick testing
-  it('click on delete contact button', () => {
-    useSelectorMock.mockReturnValue({ error: '' });
-    useDispatchMock.mockReturnValue(dispatchMock);
-
-    const contactListComponent = shallow(<ContactList {...propsWithOnClick} />);
-    const liButtonElement = contactListComponent.find('button').at(1);
-    liButtonElement.simulate('click');
-    expect(deleteContactClickMock).toHaveBeenCalledTimes(1);
   });
 });
