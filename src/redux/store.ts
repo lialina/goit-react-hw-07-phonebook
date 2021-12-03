7260e2c5e086b9cefcae6e2d87d6ba4517f2a687
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-12-03 18:38:54 +0200

    Store type checking and file extension changed from .js to .ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux'
 import contactsReducerWithSlice from './slices/contactsSlice';
+
 const rootReducer = {
   contacts: contactsReducerWithSlice,
 }
@@ -8,3 +10,8 @@ export const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV === "development",
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
