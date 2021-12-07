1d00af5e3d6f28cb065249b536dadf4ade03e0c3
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-12-07 19:49:58 +0200

    Children in ContainerProps has been provided with type JSX.Element

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Container.module.css';
 
 interface ContainerProps {
-  children: JSX.Element | JSX.Element[] | boolean | string
+  children: JSX.Element
 }
 
 export default function Container({ children }: ContainerProps) {
