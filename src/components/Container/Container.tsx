8a4d550a8133241c32a4e0a36c273b324f6433d2
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-12-07 18:49:10 +0200

    Children type in ContainerProps has been updated

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Container.module.css';
 
 interface ContainerProps {
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element | JSX.Element[] | boolean | string
 }
 
 export default function Container({ children }: ContainerProps) {
