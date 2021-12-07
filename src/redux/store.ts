d575ff771b17f0361737d5ca4e958986bd21b6cf
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-12-07 10:53:46 +0200

    Types in store.ts have been removed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,6 +10,3 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV === "development",
 })
 
-export type RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
